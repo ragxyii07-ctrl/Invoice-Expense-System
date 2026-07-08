ee60a3bab98c9e4523a11e7b0fa042741dc3f530
Ragavendhiran95 <145092781+Ragavendhiran95@users.noreply.github.com>
2026-07-08 21:39:39 +0530

    Use Render backend URL

--- frontend/src/lib/services/api.service.js
+++ frontend/src/lib/services/api.service.js
@@ -1,7 +1,7 @@
-import { auth } from '$lib/firebase/firebase';
 import { PUBLIC_API_URL } from '$env/static/public';
+import { auth } from '$lib/firebase/firebase';
 
-const API_BASE_URL = "http ://localhost :5000/api"
+const API_BASE_URL = PUBLIC_API_URL;
 
 export async function apiRequest(endpoint, options = {}) {
 	const user = auth.currentUser;
