ba885ab62e491f2cef2d220272dc96c9e48a82dc
ragxyii07-ctrl <ragxyii.07@gmail.com>
2026-07-12 18:10:16 +0530

    Update pdf.service.js
    true  .. new

--- backend/src/services/pdf.service.js
+++ backend/src/services/pdf.service.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 async function generateInvoicePdf(html) {
 	const browser = await puppeteer.launch({
-		headless: 'new',
+		headless: true,
 		args: ['--no-sandbox', '--disable-setuid-sandbox']
 	});
 
